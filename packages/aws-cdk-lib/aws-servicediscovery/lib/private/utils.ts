64bd123a4ab2e1b61fa0a5891b369d1c035ac77a
ajsaajsa <jusicajsa2@gmail.com>
2026-03-03 23:27:28 +0100

    Dodala test fajl za week3

--- packages/aws-cdk-lib/aws-servicediscovery/lib/private/utils.ts
+++
@@ -1,6 +0,0 @@
-import { INamespace, NamespaceType } from '../namespace';
-import { DiscoveryType } from '../service';
-
-export function defaultDiscoveryType(namespace : INamespace): DiscoveryType {
-  return namespace.type == NamespaceType.HTTP ? DiscoveryType.API: DiscoveryType.DNS_AND_API;
-}
--- packages/aws-cdk-lib/pipelines/test/testhelpers/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './compliance';
-export * from './modern-pipeline';
-export * from './test-app';
-export * from './matchers';
